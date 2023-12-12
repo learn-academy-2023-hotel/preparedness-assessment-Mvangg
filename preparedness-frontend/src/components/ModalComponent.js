ed47afd985adc2c4e0292dc265416e47972b0ced
LEARN Academy <learnacademy@MacBook-Air-6.local>
2023-12-12 12:41:31 -0800

    update

--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -2,21 +2,26 @@ import React, {useState} from "react";
 import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 const ModalComponent = (props) => {
+
   const [modal, setModal] = useState(false)
+
   const closeButton = (
     <button className="close" onClick={props.toggle} type="button">
-      Click
+      Close
     </button>
+
   )
   return (
-  <>
-  <Modal isOpen={modal} toggle={props.toggle} {...props}>
-    <ModalHeader close={closeButton}>Hey there {props.name}! </ModalHeader>
-    <ModalBody>
-    Grimalkin siamese kitten and thai jaguar for jaguar panther. Grimalkin maine coon maine coon scottish fold but balinese birman but siberian. American bobtail manx. Kitten. Manx american shorthair manx lynx, bombay. Norwegian forest grimalkin and tomcat tabby and american shorthair, siamese for burmese. Jaguar ragdoll himalayan. Ragdoll. Lion. Savannah american shorthair yet sphynx burmese so lion. Tabby turkish angora. Tomcat singapura. Havana brown tomcat yet munchkin, yet norwegian forest ocicat sphynx birman. Abyssinian panther or cougar for himalayan persian. Manx egyptian mau yet scottish fold or bombay, norwegian forest. Lion sphynx turkish angora, tomcat, so tabby so siamese. Tiger ocicat, siamese. Ragdoll maine coon and abyssinian . Burmese bombay. Bobcat sphynx yet munchkin for panther.
-    </ModalBody>
-  </Modal>
-  </>
+    <>
+      <Modal isOpen={modal} toggle={props.toggle} {...props}>
+        <ModalHeader close={closeButton}>
+          Hey there {props.name}! 
+        </ModalHeader>
+        <ModalBody>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </ModalBody>
+      </Modal>
+    </>
   ) 
 }
 
